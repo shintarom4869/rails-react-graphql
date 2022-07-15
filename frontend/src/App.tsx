66dc9843da141356bb93b01cac7542688219e52f
murashin <shintaro.murakami@medley.jp>
2022-07-15 18:51:12 +0900

    code genされたhooksを利用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,50 @@
-import { useQuery, gql } from "@apollo/client";
-
-const FETCH_BOOKS = gql`
-  query {
-    books {
-      id
-      title
-    }
-  }
-`;
-
-interface Book {
-  id: string;
-  title: string;
-}
+import { useState } from "react";
+import {
+  useBooksQuery,
+  useCreateBookMutation,
+  useDeleteBookMutation,
+  useUpdateBookMutation,
+} from "./graphql/generated";
 
 function App() {
-  const { data: { books = [] } = {} } = useQuery(FETCH_BOOKS);
+  const { data: { books = [] } = {} } = useBooksQuery();
+  const [createBook] = useCreateBookMutation({ refetchQueries: ["books"] });
+  const [deleteBook] = useDeleteBookMutation({ refetchQueries: ["books"] });
+  const [updateBook] = useUpdateBookMutation();
+  const [title, setTitle] = useState("");
   return (
-    <div>
-      {books.map((book: Book) => (
-        <div key={book.id}>{book.title}</div>
+    <div style={{ width: "400px", margin: " 40px auto" }}>
+      <h2>新規登録</h2>
+      <div style={{ display: "flex", gap: "10px", marginBottom: "40px" }}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button
+          onClick={() => {
+            createBook({ variables: { params: { title: title } } });
+            setTitle("");
+          }}
+        >
+          保存
+        </button>
+      </div>
+
+      <h2>書籍一覧</h2>
+      {books.map((book) => (
+        <div
+          key={book.id}
+          style={{ display: "flex", gap: "10px", marginBottom: "10px" }}
+        >
+          <input
+            value={book.title || ""}
+            onChange={(e) =>
+              updateBook({
+                variables: { id: book.id, params: { title: e.target.value } },
+              })
+            }
+          />
+          <button onClick={() => deleteBook({ variables: { id: book.id } })}>
+            削除
+          </button>
+        </div>
       ))}
     </div>
   );
