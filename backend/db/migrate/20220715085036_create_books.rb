919b2374036a666a5ee7e98e2b7cb32ca1d5ade6
murashin <shintaro.murakami@medley.jp>
2022-07-15 18:49:45 +0900

    add user model and migrate

--- backend/db/migrate/20220715085036_create_books.rb
+++ backend/db/migrate/20220715085036_create_books.rb
@@ -2,7 +2,8 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :title
-
+      t.references :user, foreign_key: true
+      t.integer :status
       t.timestamps
     end
   end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_082036) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_085036) do
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
+    t.bigint "user_id"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "users"
 end
